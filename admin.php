037fd258c9980fce3076af478497ff50d77f9c06
tremiy <562602692@qq.com>
2021-06-05 19:24:21 +0800

    adminlog

--- admin.php
+++ admin.php
@@ -4,41 +4,79 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>登录界面</title>
 <link rel="stylesheet" href="style.css" />
+
+  <link rel="stylesheet" href="./bootstrap/bootstrap.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-<div id="center">
-  <form method="post" action="Chkadmin.php">
-    <table width="211" height="111" border="0" cellpadding="0" cellspacing="0">
-      <caption>
-        用户登录
-      </caption>
-      <tr>
-        <td width="69">用户名：</td>
-        <td width="142"><label for="textfield"></label>
-        <input name="username" type="text" id="textfield" size="15" /></td>
-      </tr>
-      <tr>
-        <td>密码：</td>
-        <td><label for="textfield2"></label>
-        <input name="userpwd" type="password" id="textfield2" size="15" /></td>
-      </tr>
-      <tr>
-        <td>身份：</td>
-        <td><label for="select">
-          <select name="role" size="1">
-            <option value="admin">管理员</option>
-          </select>
-        </label></td>
-      </tr>
-      <tr>
-        <td>&nbsp;</td>
-        <td><input type="submit" name="button" id="button" value="提交" /> &nbsp;&nbsp;         
-          <input type="reset" name="button2" id="button2" value="重置" />
-        </td>
-      </tr>
-    </table>
-  </form>
-    <p>教师和学生请点击<a href="login.php">这里登陆</a></p>
-</center>
+
+
+<body>
+  <div class="wrap">
+    <div class="header">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">学生管理系统（管理员）</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+      
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">登录页
+                  <span class="visually-hidden">(current)</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="contain-wrap">
+      <div id="myForm" class="myForm">
+        <form method="post" action="Chkadmin.php">
+          <fieldset>
+            <legend>用户登录</legend>
+            <div class="form-group">
+              <label for="exampleInputEmail1" class="form-label mt-4">用户名</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                placeholder="请输入账号" name="username">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1" class="form-label mt-4">密码</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="请输入密码" name="userpwd">
+            </div>
+            <div class="form-group">
+              <label for="exampleSelect1" class="form-label mt-4">身份</label>
+              <select class="form-select" id="exampleSelect1" name="role">
+                <option value="admin">管理员</option>
+              </select>
+            </div>
+            <hr>
+            <div class="form-group set-center">
+              <button type="submit" name="button" id="button" class="btn btn-primary set-padding">提交</button>
+              <button type="reset" name="button" id="button" class="btn btn-primary set-padding">重置</button>
+            </div>
+          </fieldset>
+        </form>
+        <hr>
+        <div class="alert alert-dismissible alert-light">
+          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+          <strong>老师和学生</strong>请点击<a href="login.php" class="alert-link">这里</a>登陆
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+    </div>
+  </div>
+
+
 </body>
 </html>
