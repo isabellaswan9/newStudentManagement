be466f316c89b8f907b9798fc4e94b18b1414542
Antiklf <18318850076@163.com>
2021-06-07 10:04:14 +0800

    修改添加课程，学生，教师，上课时间，数据库文件

--- admin/SearchCourse1.php
+++ admin/SearchCourse1.php
@@ -1,58 +1,29 @@
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<<<<<<< HEAD
+<!DOCTYPE htmlPUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>查找课程</title>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<link rel="stylesheet" href="../bootstrap/bootstrap.css">
-<link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="../style.css">
-
-<!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-<!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
-<script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
 
 
-
-<title>查找课程</title>
-
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-=======
-<html xmlns="http://www.w3.org/1999/xhtml" class="h-100">
-<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>无标题文档</title>
->>>>>>> 4f66894a46707504c7443af13597aa8929527b58
 <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="../style.css">
 <script type="text/javascript" src="../bootstrap/js/bootstrap.bundle.js"></script>
 
-<<<<<<< HEAD
-<title>查找课程</title>
-</head>
 <link rel="stylesheet" href="../bootstrap/bootstrap.css" />
 <link rel="stylesheet" href="../css/style.css" />
 <link rel="stylesheet" href="../css/footer.css" />
 
 <body>
-=======
 </head>
 <body class="d-flex flex-column h-100">
->>>>>>> 4f66894a46707504c7443af13597aa8929527b58
 	<div class="wrap">
 		<div class="header">
 			<?php include("header.php"); ?>
 			<?php
-				if (!session_id()) session_start();
+				//if (!session_id()) session_start();
 				if(!isset($_SESSION['username']))
 				{
 					header("Location:../login.php");
@@ -116,11 +87,49 @@
 						</tr>
 					</thead>
 					<?php
+					function chinese($num){
+						$chinese='';
+						switch ($num){
+							case 1:
+							$chinese='一';
+							break;
+							case 2:
+							$chinese='二';
+							break;
+							case 3:
+							$chinese='三';
+							break;
+							case 4:
+							$chinese='四';
+							break;
+							case 5:
+							$chinese='五';
+							break;
+							default:
+							$chinese='';
+							
+						}
+						return $chinese;
+					}
 						if(db_num_rows($result)>0){
 							$number=db_num_rows($result);
 						for($i=0;$i<$number;$i++)
 							{
 								$row=db_fetch_array($result);
+							$num1=($row['time1']%5==0)?5:($row['time1']%5);
+							$schooltime1="周".chinese(floor($row['time1']/6+1))."第".chinese($num1)."节<br>";
+							
+							if($row['time2']){
+							$num2=($row['time2']%5==0)?5:($row['time2']%5);
+							$schooltime2="周".chinese(floor($row['time2']/6+1))."第".chinese($num2)."节<br>";
+							}
+							else $schooltime2='';
+						
+							if($row["time3"]){
+							$num3=($row['time3']%5==0)?5:($row['time3']%5);
+							$schooltime3="周".chinese(floor($row['time3']/6+1))."第".chinese($num3)."节<br>";
+							}
+							else $schooltime3='';								
 								
 								if($i%2==0)
 									echo "<tr bgcolor='#dddddd'>";
@@ -128,90 +137,8 @@
 									echo "<tr>";
 								echo "<td width='80'><a href='CourseDetail.php?CouNo=".$row['CouNo']."'>".$row['CouNo']."</a></td>";
 						?>
-<<<<<<< HEAD
-
-<?php include("header.php"); ?>
-<?php
-if(!isset($_SESSION['username']))
-{
-	header("Location:../login.php");
-	exit();
-}
-$keyWord=$_GET['keyWord'];
-$ColumnName=$_GET['ColumnName'];
-$keyWord=trim($keyWord);
-include("../conn/db_conn.php");
-include("../conn/db_func.php");
-switch($ColumnName)
-{
-	case "CouNo";
-		$sql="select * from course where CouNo LIKE \"%".$keyWord."%\"";
-		break;
-	case "CouName";
-		$sql="select * from course where CouName LIKE \"%".$keyWord."%\"";
-		break;
-	case "Kind";
-		$sql="select * from course where Kind LIKE \"%".$keyWord."%\"";
-		break;
-	case "Credit";
-		$sql="select * from course where Credit LIKE \"%".$keyWord."%\"";
-		break;
-	case "Teacher";
-		$sql="select * from course where Teacher LIKE \"%".$keyWord."%\"";
-		break;
-	case "DepartName";
-		$sql="select * from course,Department where course.DepartNo=Department.DepartNo and DepartName LIKE \"%".$keyWord."%\"";
-		break;
-	case "SchoolTime";
-		$sql="select * from course where SchoolTime LIKE \"%".$keyWord."%\"";
-		break;
-}
-$result=db_query($sql);
-?>
-
-	<?php include("header.php"); ?>
-<?php
-if (!session_id()) session_start();
-if(!isset($_SESSION['username']))
-{
-	header("Location:../login.php");
-	exit();
-}
-$keyWord=$_GET['keyWord'];
-$ColumnName=$_GET['ColumnName'];
-$keyWord=trim($keyWord);
-include("../conn/db_conn.php");
-include("../conn/db_func.php");
-switch($ColumnName)
-{
-	case "CouNo";
-		$sql="select * from course where CouNo LIKE \"%".$keyWord."%\"";
-		break;
-	case "CouName";
-		$sql="select * from course where CouName LIKE \"%".$keyWord."%\"";
-		break;
-	case "Kind";
-		$sql="select * from course where Kind LIKE \"%".$keyWord."%\"";
-		break;
-	case "Credit";
-		$sql="select * from course where Credit LIKE \"%".$keyWord."%\"";
-		break;
-	case "Teacher";
-		$sql="select * from course where Teacher LIKE \"%".$keyWord."%\"";
-		break;
-	case "DepartName";
-		$sql="select * from course,Department where course.DepartNo=Department.DepartNo and DepartName LIKE \"%".$keyWord."%\"";
-		break;
-	case "SchoolTime";
-		$sql="select * from course where SchoolTime LIKE \"%".$keyWord."%\"";
-		break;
-}
-$result=db_query($sql);
-?>
 
 
-=======
->>>>>>> 4f66894a46707504c7443af13597aa8929527b58
 					<td width="220" align="center">
 						<?php echo $row['CouName'] ?>
 					</td>
@@ -225,7 +152,7 @@ $result=db_query($sql);
 						<?php echo $row['Teacher'] ?>
 					</td>
 					<td width="120">
-						<?php echo $row['SchoolTime']  ?>
+						<?php echo $schooltime1.$schooltime2.$schooltime3;  ?>
 					</td>
 					</tr>
 
