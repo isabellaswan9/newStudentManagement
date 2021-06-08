eb2d1110ac1de5b8b7d3a2b6975db00b0823ac3c
isabellaswan9 <3206616224@qq.com>
2021-06-08 13:42:17 +0800

    初步实现批量导入excel文件功能

--- admin/AddStudent.php
+++ admin/AddStudent.php
@@ -6,9 +6,14 @@
 
 <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="../style.css">
+<script type="text/javascript" src="../bootstrap/js/jquery.min.js"></script>
 <script type="text/javascript" src="../bootstrap/js/bootstrap.bundle.js"></script>
 
 <title>添加学生</title>
+
+<script>
+  
+</script>
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -27,9 +32,9 @@ if(! isset($_SESSION["username"])){
 
 <div class="contain-wrap">
   <div class="myForm">
-    <form method="post" action="AddStudent1.php">
+    <form method="post" action="AddStudent1.php" id="myForm">
         <fieldset>
-          <legend>请输入学生信息</legend>
+          <legend>请输入学生信息 <button type="button" name="batch" id="batch" class="btn btn-primary set-padding">点此批量上传</button></legend>
           <div class="form-group">
           <label for="exampleSelect1" class="form-label mt-4">学号：</label>
             <?php
@@ -58,9 +63,58 @@ if(! isset($_SESSION["username"])){
          </div>
         </fieldset>
     </form>
+    <form method="post" action="AddStudent2.php" id="myBatch" enctype='multipart/form-data'>
+        <fieldset>
+          <legend>请上传excel文件 <button type="button" name="single" id="single" class="btn btn-primary set-padding">点击单个添加</button></legend>
+          <div id="myBatch">
+           <div class="input-group">
+              <input type="file" class="form-control" name="myfile" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+              <button class="btn btn-outline-secondary" type="submit" id="inputGroupFileAddon04">上传</button>
+            </div>
+            <div id="values"></div>
+         </div>
+         </fieldset>
+    </form>
  </div>
 </div>
+<script>
+ $(document).ready(function(){
+   $('#inputGroupFileAddon04').hide();
+    $("#myForm").show();
+    $("#myBatch").hide();
 
+    $("#batch").click(function(){
+      $("#myForm").hide();
+      $("#myBatch").show();
+    });
 
+    $("#single").click(function(){
+      $("#myForm").show();
+      $("#myBatch").hide();
+    });
+
+    $("#inputGroupFile04").change(function(){
+        var fileReader = new FileReader();
+        var file = $(this).prop('files')[0];
+        /*if (file) {
+            fileReader.readAsDataURL(file);
+        } else {
+            tipFn('请选择文件');
+            return;
+        }
+        */
+        //判断文件大小
+        if (file.size > 2000000) {
+              $("#values").text('文件大小不得超过 2 M');
+              return;
+          };
+          //将多个文件名分开
+          var fullpath = $(this).val();
+          var filename = fullpath.split('\\');
+          //显示上传按钮
+          $('#inputGroupFileAddon04').show();
+    })
+  })
+</script>
 </body>
 </html>
