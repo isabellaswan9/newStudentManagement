992b9722b022a45a7b4f12d1fb500803d41a4ccb
Atubububu <2396225771@qq.com>
2021-06-10 19:21:52 +0800

    {问题

--- tea/ShowTeached.php
+++ tea/ShowTeached.php
@@ -24,13 +24,6 @@ if(! isset($_SESSION['username']))
 	$StuNo=$_SESSION['username'];
 	$ShowCourse_sql="select * from course where Teacher in(select TeaName from Teacher where TeaNo='$StuNo') ORDER BY CouNo";
 	$ShowCourseResult=db_query($ShowCourse_sql);
-if(empty($ShowCourseResult))
-{
-	echo"<script>";
-	echo"alert(\"暂无学生选课\");";
-	echo"location.href=\"ShowCourse.php\"";
-	echo"</script>";
-}
 ?>
 <div class="contain-wrap">
 <div class="myTable">
@@ -61,77 +54,78 @@ if(empty($ShowCourseResult))
 						</tr>
 					</thead>
 <?php
-function chinese($num){
-						$chinese='';
-						switch ($num){
-							case 1:
-							$chinese='一';
-							break;
-							case 2:
-							$chinese='二';
-							break;
-							case 3:
-							$chinese='三';
-							break;
-							case 4:
-							$chinese='四';
-							break;
-							case 5:
-							$chinese='五';
-							break;
-							default:
+	function chinese($num){
 							$chinese='';
+							switch ($num){
+								case 1:
+								$chinese='一';
+								break;
+								case 2:
+								$chinese='二';
+								break;
+								case 3:
+								$chinese='三';
+								break;
+								case 4:
+								$chinese='四';
+								break;
+								case 5:
+								$chinese='五';
+								break;
+								default:
+								$chinese='';
 							
+							}
+							return $chinese;
 						}
-						return $chinese;
-					}
-if(db_num_rows($ShowCourseResult)>0){
-	$number=db_num_rows($ShowCourseResult);
+	if(db_num_rows($ShowCourseResult)>0){
+		$number=db_num_rows($ShowCourseResult);
 	
-	if(empty($_GET['p']))
-		$p=0;
-	else {$p=$_GET['p'];}	
-	$check=$p +10;
-	for($i=0;$i<$number;$i++){
-		$row=db_fetch_array($ShowCourseResult);
-		$num1=($row['time1']%5==0)?5:($row['time1']%5);
-		$schooltime1="周".chinese(floor($row['time1']/6+1))."第".chinese($num1)."节<br>";
+		if(empty($_GET['p']))
+			$p=0;
+		else {$p=$_GET['p'];}	
+		$check=$p +10;
+		for($i=0;$i<$number;$i++)
+		{
+			$row=db_fetch_array($ShowCourseResult);
+			$num1=($row['time1']%5==0)?5:($row['time1']%5);
+			$schooltime1="周".chinese(floor($row['time1']/6+1))."第".chinese($num1)."节<br>";
 							
-		if($row['time2']){
-		$num2=($row['time2']%5==0)?5:($row['time2']%5);
-		$schooltime2="周".chinese(floor($row['time2']/6+1))."第".chinese($num2)."节<br>";
-		}
-		else $schooltime2='';
+			if($row['time2']){
+				$num2=($row['time2']%5==0)?5:($row['time2']%5);
+				$schooltime2="周".chinese(floor($row['time2']/6+1))."第".chinese($num2)."节<br>";
+							}
+			else $schooltime2='';
 						
-		if($row["time3"]){
-		$num3=($row['time3']%5==0)?5:($row['time3']%5);
-		$schooltime3="周".chinese(floor($row['time3']/6+1))."第".chinese($num3)."节<br>";
-		}
-		else $schooltime3='';
+			if($row["time3"]){
+				$num3=($row['time3']%5==0)?5:($row['time3']%5);
+				$schooltime3="周".chinese(floor($row['time3']/6+1))."第".chinese($num3)."节<br>";
+				}
+			else $schooltime3='';
 		
-		if($i>=$p && $i < $check){
-			if($i%2 ==0)
-			  echo"<tr bgcolor='#DDDDDD'>";
-		else
-			  echo"<tr>";
-			  echo"<td width='80' valign='middle' align='center'>".$row['CouNo']."</td>";
-			  echo"<td width='220' valign='middle' align='center'>".$row['CouName']."</td>";
-			  echo"<td width='80' valign='middle' align='center'>".$row['Kind']."</td>";
-			  echo"<td width='50' valign='middle' align='center'>".$row['Credit']."</td>";
-			  echo"<td width='80' valign='middle' align='center'>".$row['Teacher']."</td>";
-			  echo"<td width='100' valign='middle' align='center'>".$schooltime1.$schooltime2.$schooltime3."</td>";
-		 	  /*点击查看可以查看学生名单以及录入成绩*/		  
-			  echo"<td width='80' valign='middle' align='center'><a href='Showstudent.php?CouNo=".$row['CouNo']."'>查看学生</a></td>";
+			if($i>=$p && $i < $check){
+				if($i%2 ==0)
+				  echo"<tr bgcolor='#DDDDDD'>";
+			else
+				  echo"<tr>";
+				  echo"<td width='80' valign='middle' align='center'>".$row['CouNo']."</td>";
+				  echo"<td width='220' valign='middle' align='center'>".$row['CouName']."</td>";
+				  echo"<td width='80' valign='middle' align='center'>".$row['Kind']."</td>";
+				  echo"<td width='50' valign='middle' align='center'>".$row['Credit']."</td>";
+				  echo"<td width='80' valign='middle' align='center'>".$row['Teacher']."</td>";
+				  echo"<td width='100' valign='middle' align='center'>".$schooltime1.$schooltime2.$schooltime3."</td>";
+			 	  /*点击查看可以查看学生名单以及录入成绩*/		  
+				  echo"<td width='80' valign='middle' align='center'><a href='Showstudent.php?CouNo=".$row['CouNo']."'>查看学生</a></td>";
 
-			  echo"</tr>";
-			  $j=$i+1; 
-		 }
+				  echo"</tr>";
+				  $j=$i+1; 
+		 		}
 		}
 
 	}
 	else{
 				echo"<script>";
-				echo"alert(\"暂无学生选课\");";
+				echo"alert(\"暂无课程安排！\");";
 				echo"location.href=\"ShowCourse.php\"";
 				echo"</script>";
 				}
